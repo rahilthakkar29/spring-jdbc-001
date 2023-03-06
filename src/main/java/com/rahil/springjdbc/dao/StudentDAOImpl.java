9eda5bfae454f0cd078802a5d2114edf90df0649
rahilthakkar29 <thakkarrahil52@gmail.com>
2023-03-06 16:36:03 -0500

    BatchPreparedStatementSetter added

--- src/main/java/com/rahil/springjdbc/dao/StudentDAOImpl.java
+++ src/main/java/com/rahil/springjdbc/dao/StudentDAOImpl.java
@@ -3,10 +3,13 @@ package com.rahil.springjdbc.dao;
 import com.rahil.springjdbc.domain.Student;
 import com.rahil.springjdbc.resultsetextractor.StudentResultSetExtractor;
 import com.rahil.springjdbc.rowmapper.StudentRowMapper;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 
 @Repository("studentDao")
@@ -118,6 +121,54 @@ public class StudentDAOImpl implements StudentDAO {
         }
         studentMap.forEach((key,value) -> System.out.println(key + " :: { " + value + " }"));
         System.out.println(studentMap);
+    }
+
+    @Override
+    public int updateStudent(Student student) {
+        String sql = "UPDATE STUDENT SET STUDENT_ADDRESS = ?" +
+                " WHERE ROLL_NO = ?";
+
+        Object[] arg = {student.getAddress(), student.getRollNo()};
+
+        return jdbcTemplate.update(sql, arg);
+    }
+
+
+    @Override
+    public void updateStudent(List<Student> students) {
+        // This is my prepared statement
+        // Which will send to the db by application then db will compile this ps
+        // that compiled ps returns and execute the queries
+        String sql = "UPDATE STUDENT SET STUDENT_ADDRESS = ?" +
+                " WHERE ROLL_NO = ?";
+
+        jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps, int i) throws SQLException {
+                // we need to set the args for the prepared statement
+
+                ps.setString(1, students.get(i).getAddress());
+                ps.setInt(2, students.get(i).getRollNo());
+            }
+
+            @Override
+            public int getBatchSize() {
+                // in this method we need to define how many times our query will execute
+                // how many times the setValues() is going to execute?
+                return students.size();
+            }
+        });
+/*
+        List<Object[]> args = new ArrayList<>();
+
+        for (Student temp : students) {
+            Object[] tempArg = {temp.getAddress(), temp.getRollNo()};
+            args.add(tempArg);
+        }
+
+        jdbcTemplate.batchUpdate(sql, args);
+
+        System.out.println("Done");*/
     }
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
